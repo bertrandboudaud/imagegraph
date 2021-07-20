e1fed9bd37072a379f59565bcd9de1445a578db6
Bertrand Boudaud <bertrand.boudaud@gmail.com>
2021-07-20 20:13:53 +0200

    using form

--- client/src/components/Ping.vue
+++ client/src/components/Ping.vue
@@ -24,6 +24,37 @@
         <br>
         <button type="button" class="btn btn-success btn-sm">Lanch workflow</button>
         <br>
+        <b-form @submit="onSubmit" @reset="onReset">
+          <b-form-group id="form-title-group"
+                    label="Title:"
+                    label-for="form-title-input">
+            <b-form-input id="form-title-input"
+                        type="text"
+                        v-model="addBookForm.title"
+                        required
+                        placeholder="Enter title">
+            </b-form-input>
+          </b-form-group>
+          <b-form-group id="form-author-group"
+                      label="Author:"
+                      label-for="form-author-input">
+            <b-form-input id="form-author-input"
+                          type="text"
+                          v-model="addBookForm.author"
+                          required
+                          placeholder="Enter author">
+            </b-form-input>
+          </b-form-group>
+          <b-form-group id="form-read-group">
+            <b-form-checkbox-group v-model="addBookForm.read" id="form-checks">
+              <b-form-checkbox value="true">Read?</b-form-checkbox>
+            </b-form-checkbox-group>
+          </b-form-group>
+        <b-button-group>
+          <b-button type="submit" variant="primary">Submit</b-button>
+          <b-button type="reset" variant="danger">Reset</b-button>
+        </b-button-group>
+      </b-form>
       </div>
     </div>
   </div>
@@ -36,6 +67,11 @@ export default {
   data() {
     return {
       inputs: [],
+      addBookForm: {
+        title: '',
+        author: '',
+        read: [],
+      },
     };
   },
   methods: {
@@ -59,6 +95,41 @@ export default {
         default:
           return 'prout';
       }
+    },
+    addBook(payload) {
+      const path = 'http://localhost:5000/books';
+      axios.post(path, payload)
+        .then(() => {
+          this.getBooks();
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.log(error);
+          this.getBooks();
+        });
+    },
+    initForm() {
+      this.addBookForm.title = '';
+      this.addBookForm.author = '';
+      this.addBookForm.read = [];
+    },
+    onSubmit(evt) {
+      evt.preventDefault();
+      this.$refs.addBookModal.hide();
+      let read = false;
+      if (this.addBookForm.read[0]) read = true;
+      const payload = {
+        title: this.addBookForm.title,
+        author: this.addBookForm.author,
+        read, // property shorthand
+      };
+      this.addBook(payload);
+      this.initForm();
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      this.$refs.addBookModal.hide();
+      this.initForm();
     },
   },
   created() {
